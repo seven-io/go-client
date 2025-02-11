67e4993f1aafac210c2a19b6174e6bdd14969d21
Florian Vogt <florian.vogt@puresec.de>
2025-02-11 15:34:08 +0100

    Fix: Voice response cannot be decoded

--- seven/voice_test.go
+++ seven/voice_test.go
@@ -1,8 +1,10 @@
 package seven
 
 import (
-	a "github.com/stretchr/testify/assert"
+	"encoding/json"
 	"testing"
+
+	a "github.com/stretchr/testify/assert"
 )
 
 func TestVoiceResource_Dispatch(t *testing.T) {
@@ -29,4 +31,52 @@ func TestVoiceResource_Dispatch(t *testing.T) {
 	} else {
 		a.Nil(t, v)
 	}
+}
+
+func TestVoice_UnmarshalJSON(t *testing.T) {
+	// The following example is taken from the API documentation and should be able to be transformed without errors.
+	// See: https://docs.seven.io/de/rest-api/endpunkte/voice
+	apiExample := []byte(`
+{
+  "success": "100",
+  "total_price": 0.045,
+  "balance": 3509.236,
+  "debug": false,
+  "messages": [
+    {
+      "id": 1384013,
+      "sender": "sender",
+      "recipient": "49176123456789",
+      "text": "Hallo Welt!",
+      "price": 0.045,
+      "success": true,
+      "error": null,
+      "error_text": null
+    }
+  ]
+}`)
+
+	js := &Voice{}
+	err := json.Unmarshal(apiExample, js)
+	if a.NoError(t, err) {
+		a.Len(t, js.Messages, 1)
+	}
+}
+
+func TestVoiceHangup_UnmarshalJSON(t *testing.T) {
+	// The following example is taken from the API documentation and should be able to be transformed without errors.
+	// See: https://docs.seven.io/de/rest-api/endpunkte/voice
+	apiExample := []byte(`
+{
+  "success": true,
+  "error": null
+}`)
+
+	js := &VoiceHangup{}
+	err := json.Unmarshal(apiExample, js)
+	if a.NoError(t, err) {
+		a.True(t, js.Success)
+		a.Nil(t, js.Error)
+	}
+
 }
